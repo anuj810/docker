957057fce4f68264e3baf8a049d4bb2940eed75b
anuj810 <anuj11102190@gmail.com>
2018-03-06 12:32:31 +0000

    app1

--- app1/server.js
+++ app1/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // // App
 const app = express();
 app.get('/', (req, res) => {
-res.send('Hello world from app\n');
+res.send('Hello world from app1\n');
    });
 
    app.listen(PORT, HOST);
